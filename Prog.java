972cac633f75f771fceb9e28d38dbba1c8d3592c
root <root@ip-172-31-15-67.ap-south-1.compute.internal>
2020-03-27 09:47:26 +0000

    modified

--- Prog.java
+++ Prog.java
@@ -4,7 +4,7 @@ public class Prog{
 
 			System.out.println("Welcome to my 1st Jenkins Program"+i);
 		}	
-		System.out.println("Program executed ");
+		System.out.println("Program executed ")
 		
 	}
 }
