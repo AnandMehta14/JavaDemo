12b139d245f50dfc3acdc4fc69fe722461cb6b56
root <root@ip-172-31-15-67.ap-south-1.compute.internal>
2020-03-27 09:05:15 +0000

    Modified

--- Prog.java
+++ Prog.java
@@ -1,6 +1,6 @@
 public class Prog{
 	public static void main(String[] args){
-		for(int i=1;i<=10;i++){
+		for(int i=1;i<=20;i++){
 			System.out.println("Welcome to my 1st Jenkins Program"+i);
 		}	
 		System.out.println("Program executed ");
